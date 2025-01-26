43a8557bda651cb287ecc8f0cbd3b1a4af345682
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-01-26 03:06:38 +0200

    Read pdf and printing

--- src/main/java/rest/reservoirapi/appConfig/PdfReader.java
+++ src/main/java/rest/reservoirapi/appConfig/PdfReader.java
@@ -6,23 +6,59 @@ import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class PdfReader {
 
     public void readPdf(String filepath) throws IOException {
         String filePath = "C:/Users/Nikolay/Desktop/Rest/reservoirApi/src/main/resources/static/22012025_Bulletin_Daily.pdf";
+//        String filePath = "C:/Users/Nikolay/Documents/GitHub/reservoirAPI/src/main/resources/static/07012025_Bulletin_Daily.pdf";
+//        String filePath = "C:/Users/Nikolay/Documents/GitHub/reservoirAPI/src/main/resources/static/07012025_Bulletin_Daily.pdf";
         File file = new File(filePath);
         try {
             PDDocument document = Loader.loadPDF(file);
             PDFTextStripper pdfTextStripper = new PDFTextStripper();
             pdfTextStripper.setStartPage(3);
-            pdfTextStripper.setEndPage(4);
+            pdfTextStripper.setEndPage(5);
 
             String text = pdfTextStripper.getText(document);
-            System.out.println(text);
+            String[] split = text.split(" ");
+            int indexStart = 194;
+            System.out.println(LocalDate.now());
+
+//            for (int i = 1; i <= 65; i++) {
+//                System.out.println("Язовир номер " + i);
+//                System.out.println("Име язовир " + split[indexStart++]);
+//                if (isWord(split[indexStart])) {
+//                    System.out.println(indexStart);
+//                    System.out.println("Име язовир " + split[indexStart]);
+//                    indexStart++;
+//                }
+//                System.out.println("Общ обем " + split[indexStart++]);
+//                System.out.println("Мъртъв Санитарен обем " + split[indexStart++]);
+//                System.out.println("Наличен обем");
+//                System.out.println("млн.м3 " + split[indexStart++]);
+//                System.out.println("% от общия обем  " + split[indexStart++]);
+//                System.out.println("Наличен полезен обем");
+//                System.out.println("млн.м3 " + split[indexStart++]);
+//                System.out.println("% от полезния обем " + split[indexStart++]);
+//                System.out.println("Ср. денонощен приток " + split[indexStart++]);
+//                System.out.println("Ср. денонощен разход " + split[indexStart++]);
+//                System.out.println("ТЕНДЕНЦИЯ - " + split[indexStart++]);
+//                indexStart = indexStart + 2;
+//            }
+
+            System.out.println(split.length);
+
             document.close();
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean isWord(String word) {
+        boolean matches = word.matches("[а-яА-Я]+");
+        System.out.println(matches);
+        return matches;
     }
 }
