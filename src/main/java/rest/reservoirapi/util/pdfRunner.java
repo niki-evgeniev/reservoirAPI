d8bc2f2f0b9dfbebf25d8951871122d62dff6a80
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-02-08 02:25:11 +0200

    fix runner

--- src/main/java/rest/reservoirapi/util/pdfRunner.java
+++ src/main/java/rest/reservoirapi/util/pdfRunner.java
@@ -4,28 +4,23 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import rest.reservoirapi.service.impl.DownloadFileService;
 import rest.reservoirapi.service.impl.PdfReaderService;
-import rest.reservoirapi.service.impl.PdfReaderServiceTest;
 
 @Component
 public class pdfRunner implements CommandLineRunner {
 
-    private final PdfReaderService pdfReaderService;
     private final DownloadFileService downloadFile;
-    private final PdfReaderServiceTest pdfReaderServiceTest;
+    private final PdfReaderService pdfReaderService;
 
-    public pdfRunner(PdfReaderService pdfReaderService, DownloadFileService downloadFile, PdfReaderServiceTest pdfReaderServiceTest) {
-        this.pdfReaderService = pdfReaderService;
-
+    public pdfRunner( DownloadFileService downloadFile, PdfReaderService pdfReaderService) {
         this.downloadFile = downloadFile;
-        this.pdfReaderServiceTest = pdfReaderServiceTest;
+        this.pdfReaderService = pdfReaderService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         String fileName = downloadFile.downloadReservoirInfo();
         Thread.sleep(1000);
-//        pdfReaderService.readPdf(fileName);
-        pdfReaderServiceTest.readPdf(fileName);
+        pdfReaderService.readPdf(fileName);
 
     }
 }
