348a00434a12c1950e42e7d02926212b917263ae
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-02-12 03:10:55 +0200

    Add Scheduling

--- src/main/java/rest/reservoirapi/ReservoirApiApplication.java
+++ src/main/java/rest/reservoirapi/ReservoirApiApplication.java
@@ -2,8 +2,10 @@ package rest.reservoirapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ReservoirApiApplication {
 
     public static void main(String[] args) {
