d78df4d0c8935da4403320e344991d166080e9dc
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-02-12 03:09:02 +0200

    Add folder to download

---
+++ src/main/java/rest/reservoirapi/appConfig/WebAppConfiguration.java
@@ -0,0 +1,15 @@
+package rest.reservoirapi.appConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAppConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/Download/**")
+                .addResourceLocations("file:./Download/");
+    }
+}
